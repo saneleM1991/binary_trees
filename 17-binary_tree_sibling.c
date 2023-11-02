170b72624e698aeb1ab796cea22a469ff26704d8
saneleM1991 <91sanelem@gmail.com>
2023-11-02 15:39:48 +0200

    17 and 18 fixer 1

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->left == node)
+	if (node->parent == node)
 		return (node->parent->right);
-	return (node->parent->left);
+	return (node->parent);
 }
